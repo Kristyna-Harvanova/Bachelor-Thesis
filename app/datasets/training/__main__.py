d6af2dea96fb6d7fe0b54778c27fb52d9d10dedb
Kristýna Harvanová <77255881@cuni.cz>
2024-03-05 21:41:24 +0100

    add TODO

--- app/datasets/training/__main__.py
+++ app/datasets/training/__main__.py
@@ -7,6 +7,8 @@ def main():
     # Convert the MSCX files to .svg
     #convert_mscx2format("datasets\\OpenScoreLieder\\scores", "svg")
 
+    #TODO udelat i pro png, ale nefunguje pro vsechny u svg (viz convert_mscx2format)
+
     # Extract the annotations from the SVG 
     svg_files = glob.glob(r"datasets\OpenScoreLieder\scores\*\*\*\*.svg", recursive=True)
 
@@ -14,6 +16,8 @@ def main():
         directory = os.path.dirname(svg_file.replace(".svg", ".json").replace("scores", "annotations"))
         os.makedirs(directory, exist_ok=True)
         extract_annotations_from_mscore_svg(svg_file, directory + "\\" + os.path.basename(svg_file).replace(".svg", ".json"))
+    
+    #TODO: nechat bezet a vytvorit vsecny jsony (trva to), az budu mit dostupne vsechny svg viz convert_mscx2format
 
 if __name__ == "__main__":
     main()
