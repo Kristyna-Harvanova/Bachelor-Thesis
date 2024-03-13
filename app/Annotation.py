a3360775ccb7a6f6fc2a7920fb059d8ed2c0628d
Kristýna Harvanová <77255881@cuni.cz>
2024-03-13 11:55:11 +0100

    make dataclass

--- app/Annotation.py
+++ app/Annotation.py
@@ -1,19 +1,15 @@
-class Annotation:
-    CLASSES = ["Notehead", "Staff", "StaffMeasure"]  #TODO: pridat do skriptu obou, kde se vola pro cele pole
+from dataclasses import dataclass
+from typing import ClassVar, List
 
-    def __init__(
-        self, 
-        cls: str, 
-        x: int, 
-        y: int, 
-        width: int, 
-        height: int
-    ):
-        self.cls = cls
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+@dataclass
+class Annotation:
+    CLASSES: ClassVar[List[str]] = ["Notehead", "Staff", "StaffMeasure"]  #TODO: pridat do skriptu obou, kde se vola pro cele pole
+
+    cls: str
+    x: int
+    y: int 
+    width: int
+    height: int
     
     def to_json(self) -> dict:
         return {
